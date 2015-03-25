a04bb9a05cb93f737181fdeb8157c711b4844441
Tod Olson <tod@uchicago.edu>
2015-03-25 15:00:46 -0500

    initial frame for docstore holdings tree timings

---
+++ docstore/holdings-tree-timing.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import time
+import urllib
+
+search_table = [
+    {'name': 'Bib 831: 3 piano pieces / Chopin', 'bib_id':831},
+    {'name': 'Bib 3', 'bib_id':3},
+]
+
+def holdings_tree_search(base, bibId):
+    params = urllib.urlencode({'bibId':bib_id})
+    url = "%s?%s" % (base, params)
+    # print url
+    start_time = time.clock()
+    f = urllib.urlopen(url)
+    end_time = time.clock()
+    results = {'query':'bibId=%d' % bib_id,
+               'time':end_time - start_time, 'code':f.getcode()}
+    return results
+
+def print_result(name, results):
+    print "%s\t%s\t%f" % (name, results['code'], results['time'])
+    
+if __name__ == '__main__':
+
+    # http://raspberry.lib.uchicago.edu:8080/oledocstore/documentrest/holdings/tree?bibId=831
+
+    host = 'ole.uchicago.edu'
+    base = 'http://raspberry.lib.uchicago.edu:8080/oledocstore/documentrest/holdings/tree'
+    
+    for s in search_table:
+        bib_id = s['bib_id']
+        name = s['name']
+        print_result(name, holdings_tree_search(base, bib_id,))
+    
